b80b6f306b9f71c019bd0a08ef1fecdadce3c695
Amir Ashraf Izham <amirashrafizham@Amirs-MacBook-Pro.local>
2022-04-20 06:56:44 +0800

    FastAPI Course: QueryParameters

--- FastAPI_SQLAlchemy_PostgreSQL/main.py
+++ FastAPI_SQLAlchemy_PostgreSQL/main.py
@@ -1,13 +1,38 @@
+from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
 
+"""
+STATIC ROUTING (ENDPOINTS WITHOUT ID). YOU HAVE TO PUT THESE ROUTING BEFORE DYNAMIC ONES,
+ELSE IT WILL RETURN AN ERROR
+"""
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'Sarthak'}}
+
+@app.get('/blog')
+def index(limit: int, published: bool, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs queried'}
+    else:
+        return {'data': f'{limit} blogs queried'}
 
 
-@app.get('/about')
-def about():
-    return {'data': 'about page'}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'unpublished blog list'}
+
+
+"""
+DYNAMIC ROUTING (ENDPOINTS WITH ID). YOU HAVE TO PUT THESE ROUTING BEFORE DYNAMIC ONES,
+ELSE IT WILL RETURN AN ERROR
+"""
+
+
+@app.get('/blog/{id}')
+def about(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/commments')
+def comments(id: int, limit: int = 10):
+    return {'data': f'{10} comments for id:{id}'}
